066919abd57a5d977af68546e0c0d7247be2654c
Asterios Ntinos <ntinosasterios@gmail.com>
2020-07-10 23:37:59 +0300

    Center search bar h and v

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container
+      class="vh-100 text-center justify-content-center align-items-center"
+      style="display: flex"
+    >
+      <div id="content" style="width:100%">
+        <b-row class="justify-content-center">
+          <b-col>
+            <h1>Search anything :)</h1>
+          </b-col>
+        </b-row>
+        <b-row class="justify-content-center">
+          <b-col>
+            <b-form>
+              <b-input-group style="margin : auto; width : 50%">
+                <b-form-input id="searchInput" type="text" required placeholder="Search here..."></b-form-input>
+                <b-input-group-append>
+                  <b-button type="submit" variant="primary">Go!</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form>
+          </b-col>
+        </b-row>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
