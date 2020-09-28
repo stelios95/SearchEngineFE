c32f3c49ffc9c84e87a502edebfde0b3612d1928
Asterios Ntinos <ntinosasterios@gmail.com>
2020-09-29 01:40:57 +0300

    [FEAT] change background image from sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <!-- :style="{'background-image': `url(${require('./assets/landscape2.jpg')})`}" -->
+  <div id="app" v-bind:style="{backgroundImage: bgrdImageUrl }">
     <b-container
       class="vh-100 text-center justify-content-center align-items-center"
       style="display: flex"
@@ -8,7 +7,7 @@
       <div id="content" style="width:100%">
         <b-row class="justify-content-center">
           <b-col>
-            <h1 :style="{textStyle}">Search anything :)</h1>
+            <h1 v-bind:style="{color: textStyle}">Search anything :)</h1>
           </b-col>
         </b-row>
         <b-row class="justify-content-center">
@@ -17,7 +16,7 @@
               <b-input-group style="margin : auto; width : 50%">
                 <b-form-input id="searchInput" type="text" required placeholder="Search here..."></b-form-input>
                 <b-input-group-append>
-                  <b-button type="submit" variant="primary">Go!</b-button>
+                  <b-button type="submit" name="search" variant="primary">Go!</b-button>
                 </b-input-group-append>
               </b-input-group>
             </b-form>
@@ -25,10 +24,27 @@
         </b-row>
         <b-row class="justify-content-center mt-3">
           <b-col>
-            <b-button class="mr-2" variant="info">Customize search</b-button>
+            <b-button class="mr-2" v-b-toggle.sidebar-1 variant="info">Customize search</b-button>
             <b-button variant="info">I'm feeling lucky</b-button>
           </b-col>
         </b-row>
+        <!-- CUSTOMIZATION SIDEBAR -->
+        <b-sidebar id="sidebar-1" title="Customize search" shadow>
+          <div class="px-3 py-2">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              <b-form-group id="input-group-1" label="Select Background:" label-for="input-1">
+                <b-form-select
+                  id="input-1"
+                  v-model="form.bgrdImage"
+                  :options="bgrdImageOptions"
+                  required
+                ></b-form-select>
+              </b-form-group>
+              <b-button type="submit" name="submitCustomization" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+          </div>
+        </b-sidebar>
       </div>
     </b-container>
   </div>
@@ -39,11 +55,42 @@ export default {
   data() {
     return {
       hasBackground: false,
+      form: {
+        bgrdImage: 0,
+      },
+      bgrdImageOptions: [
+        { value: 0, text: "None" },
+        { value: 1, text: "Theme 1" },
+        { value: 2, text: "Theme 2" },
+      ],
+      show: true,
+      bgrdImageUrl: "",
     };
+  },
+  methods: {
+    onSubmit(ev) {
+      ev.preventDefault();
+      this.hasBackground = this.form.bgrdImage ? true : false;
+      this.changeBackgroundImage();
+    },
+    onReset(ev) {
+      ev.preventDefault();
+      this.form.bgrdImage = 0;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+    changeBackgroundImage() {
+      if (this.form.bgrdImage === 0) this.bgrdImageUrl = "";
+      if (this.form.bgrdImage === 1)
+        this.bgrdImageUrl = `url(${require("./assets/landscape1.jpg")})`;
+      if (this.form.bgrdImage === 2)
+        this.bgrdImageUrl = `url(${require("./assets/landscape2.jpg")})`;
+    },
   },
   computed: {
     textStyle() {
-      return this.hasBackground ? "#color: #d3d5de" : "#color: #f5f6f7";
+      return this.hasBackground ? "#f5f6f7" : "#141414";
     },
   },
 };
