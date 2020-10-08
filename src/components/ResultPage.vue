126c2856be78bc9b96a126031779c90fadd2144e
Asterios Ntinos <ntinosasterios@gmail.com>
2020-10-08 23:11:52 +0300

    [FEAT] search redirect to results

---
+++ src/components/ResultPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Result Page and {{ param }}</h1>
+    <p>Results: {{ searchResults }}</p>
+    <ResultItem></ResultItem>
+  </div>
+</template>
+
+<script>
+import ResultItem from "./ResultItem";
+export default {
+  components: { ResultItem },
+  props: ["searchConfigs"],
+  data() {
+    return {
+      param: this.searchConfigs,
+      searchResults: null,
+    };
+  },
+  methods: {
+    async performSearch() {
+      try {
+        const response = await this.axios.post(
+          "http://localhost:5000/searchApi/search",
+          this.searchConfigs
+        );
+        this.searchResults = response;
+        console.log(JSON.stringify(response));
+        if (this.searchConfigs.isLucky) this.luckyRedirect();
+      } catch (err) {
+        this.searchResults = null;
+        console.log(err);
+      } finally {
+        console.log("finally");
+      }
+    },
+
+    luckyRedirect() {
+      if (this.searchResults.data.length) {
+        const url = this.searchResults.data[0].loc;
+        window.location.href = url;
+      }
+    },
+  },
+  mounted() {
+    this.performSearch();
+  },
+};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,37 @@
 import Vue from "vue";
 import App from "./App.vue";
 import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
+import VueRouter from "vue-router";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueAxios from "vue-axios";
 import axios from "axios";
 
+import HomePage from "./components/HomePage.vue";
+import ResultPage from "./components/ResultPage.vue";
+
 Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  {
+    path: "/search/:searchConfigs",
+    name: "search",
+    component: ResultPage,
+    props: true,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+new Vue(Vue.util.extend({ router }, App)).$mount("#app");
