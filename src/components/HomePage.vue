9f4258e681c89f7b252a83fa10dfa6f6e398b8a1
Asterios Ntinos <ntinosasterios@gmail.com>
2020-12-06 02:46:19 +0200

    [FEAT] mobile friendly home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,7 +4,42 @@
       class="vh-100 text-center justify-content-center align-items-center"
       style="display: flex"
     >
-      <div id="content" style="width: 100%">
+      <div v-if="isMobile" style="width: 100%">
+        <b-row class="justify-content-center">
+          <b-col>
+            <h1 v-bind:style="{ color: textStyle }">Search anything :)</h1>
+          </b-col>
+        </b-row>
+        <b-row class="justify-content-center"
+          ><b-form @submit="onSearchSubmit">
+            <b-input-group style="margin: auto; width: 100%">
+              <b-form-input
+                v-model="searchConfigs.searchTerm"
+                id="searchInput"
+                type="text"
+                required
+                placeholder="Something interesting..."
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button type="submit" name="search" variant="primary"
+                  >Go!</b-button
+                >
+              </b-input-group-append>
+            </b-input-group>
+          </b-form>
+        </b-row>
+        <b-row class="justify-content-center mt-3"
+          ><b-button class="mt-2" v-b-toggle.sidebar-1 variant="info"
+            >Customize search</b-button
+          >
+        </b-row>
+        <b-row class="justify-content-center mt-3"
+          ><b-button class="mt-2" variant="info" v-on:click="luckySearch"
+            >I'm feeling lucky</b-button
+          >
+        </b-row>
+      </div>
+      <div v-else style="width: 100%">
         <b-row class="justify-content-center">
           <b-col>
             <h1 v-bind:style="{ color: textStyle }">Search anything :)</h1>
@@ -40,34 +75,34 @@
             >
           </b-col>
         </b-row>
-        <!-- CUSTOMIZATION SIDEBAR -->
-        <b-sidebar id="sidebar-1" title="Customize search" shadow>
-          <div class="px-3 py-2">
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-              <b-form-group
-                id="input-group-1"
-                label="Select Background:"
-                label-for="input-1"
-              >
-                <b-form-select
-                  id="input-1"
-                  v-model="form.bgrdImage"
-                  :options="bgrdImageOptions"
-                  required
-                ></b-form-select>
-              </b-form-group>
-              <b-button
-                class="mr-4"
-                type="submit"
-                name="submitCustomization"
-                variant="primary"
-                >Submit</b-button
-              >
-              <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-          </div>
-        </b-sidebar>
       </div>
+      <!-- CUSTOMIZATION SIDEBAR -->
+      <b-sidebar id="sidebar-1" title="Customize search" shadow>
+        <div class="px-3 py-2">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+              id="input-group-1"
+              label="Select Background:"
+              label-for="input-1"
+            >
+              <b-form-select
+                id="input-1"
+                v-model="form.bgrdImage"
+                :options="bgrdImageOptions"
+                required
+              ></b-form-select>
+            </b-form-group>
+            <b-button
+              class="mr-4"
+              type="submit"
+              name="submitCustomization"
+              variant="primary"
+              >Submit</b-button
+            >
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+        </div>
+      </b-sidebar>
     </b-container>
   </div>
 </template>
@@ -139,6 +174,17 @@ export default {
   computed: {
     textStyle() {
       return this.hasBackground ? "#f5f6f7" : "#141414";
+    },
+    isMobile() {
+      return navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+        ? true
+        : false;
     },
   },
 
